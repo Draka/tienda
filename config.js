b89f7c019196be7cd8158b16715543d35d13b593
Juan Carlos Martínez Rey <juan.mar@gmail.com>
2020-12-21 21:52:43 -0500

    checking 2 paso

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const tenancy = process.env.TENANCY || 'vendelomejor';
 const tenancyConfig = require(`./tenancy/${tenancy}`);
 
 const enviroment = {
-  v: '2.0.0',
+  v: '2.0.1',
   tenancy,
   db: process.env.MONGO_URL || 'mongodb://localhost/tienda',
   redis: {
