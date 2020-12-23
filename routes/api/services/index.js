8b8382968de478c88d7b561e6fe40994bb27d9b5
Juan Carlos Martínez Rey <juan.mar@gmail.com>
2020-12-23 17:57:35 -0500

    prefijo cache

--- routes/api/services/index.js
+++ routes/api/services/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable global-require */
 module.exports = (app) => {
   app.get('/v1/services/geocoder', redisMiddleware, require('./geocoder'));
-  app.get('/v1/services/send-email', redisMiddleware, require('./send-email'));
-  app.get('/v1/services/check-sqs', redisMiddleware, require('./check-sqs'));
+  app.get('/v1/services/send-email', require('./send-email'));
+  app.get('/v1/services/check-sqs', require('./check-sqs'));
 };
