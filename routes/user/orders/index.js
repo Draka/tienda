2445356ff11d20d557d1640312a713471088c370
Juan Carlos Martínez Rey <juan.mar@gmail.com>
2021-01-05 20:56:36 -0500

    checkAuth

--- routes/user/orders/index.js
+++ routes/user/orders/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable global-require */
 module.exports = (app) => {
-  app.get('/usuario/pedidos', checkAuthAdminStore, require('./list'));
-  app.get('/usuario/pedidos/:orderID', checkAuthAdminStore, require('./view'));
-  app.get('/usuario/ordenes/cancelar-confirmacion', checkAuthAdminStore, require('./cancelar-confirmacion'));
+  app.get('/usuario/pedidos', checkAuth, require('./list'));
+  app.get('/usuario/pedidos/:orderID', checkAuth, require('./view'));
+  app.get('/usuario/ordenes/cancelar-confirmacion', checkAuth, require('./cancelar-confirmacion'));
 };
