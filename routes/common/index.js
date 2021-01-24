43627877d6520ef28246f50a2d0765b4601441f5
Juan Carlos Martínez Rey <juan.mar@gmail.com>
2021-01-24 18:57:21 -0500

    images

--- routes/common/index.js
+++ routes/common/index.js
@@ -1,6 +1,6 @@
 /* eslint-disable global-require */
 module.exports = (app) => {
-  if (appCnf.tenancy == 'vendelomejor') {
+  if (appCnf.tenancy !== 'vendelomejor') {
     app.get('/', require('./landpage'));
   }
   // app.get('/', require('./landpage'));
