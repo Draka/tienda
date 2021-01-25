d936556685971c20b814da202272ee7146b0dff2
Juan Carlos Martínez Rey <juan.mar@gmail.com>
2021-01-25 15:31:10 -0500

    método para ver si la ttienda tiene problemas de publicación

--- appCnf.js
+++ appCnf.js
@@ -6,7 +6,7 @@
 const tenancy = process.env.TENANCY || 'vendelomejor';
 
 const enviroment = {
-  v: '2.0.10',
+  v: '2.0.11',
   tenancy,
   db: process.env.MONGO_URL || 'mongodb://localhost/tienda',
   redis: {
