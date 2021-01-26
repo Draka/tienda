b792b3f12b8d8959436f76d26f324ec8014c75f1
Juan Carlos Martínez Rey <juan.mar@gmail.com>
2021-01-26 11:58:17 -0500

    .

--- modules/site/controllers/cron/stores-error.js
+++ modules/site/controllers/cron/stores-error.js
@@ -19,24 +19,24 @@ module.exports = (req, res, next) => {
         checkStore(store, cb);
       }, cb);
     }],
-    mailer: ['check', (results, cb) => {
-      async.eachLimit(results.query, 10, (store, cb) => {
-        sqsMailer({
-          noNow: true,
-          to: { email: store.userID.email, name: store.userID.personalInfo.name },
-          subject: 'Completa la información de tu tienda',
-          template: 'msg-general',
-          msg: `<p>Desde el equipo de ${appCnf.site.name} agradecemos el uso de nuestra aplicación.</p>`
-          + `<p>Hemos encontrado algunos errores que deben ser corregidos en brevedad. Por favor, Ingresa a ${appCnf.site.url}`
-          + ' y en la lista de tiendas aparecerá con un ícono rojo los problemas encontrados y una explicación de cómo puedes corregirlos.</p>'
-          + `<p>En ${appCnf.site.name} estamos comprometidos con la calidad de las tiendas aliadas que trabajan con nosotros,`
-          + ' recuerda que si tienes dudas adicionales, puedes contactarnos directamente por WhatsApp en el número'
-          + ` ${(_.find(appCnf.site.contacts, { slug: 'whatsapp' })).value}.</p>`,
-        },
-        { _id: store.userID._id },
-        cb);
-      }, cb);
-    }],
+    // mailer: ['check', (results, cb) => {
+    //   async.eachLimit(results.query, 10, (store, cb) => {
+    //     sqsMailer({
+    //       noNow: true,
+    //       to: { email: store.userID.email, name: store.userID.personalInfo.name },
+    //       subject: 'Completa la información de tu tienda',
+    //       template: 'msg-general',
+    //       msg: `<p>Desde el equipo de ${appCnf.site.name} agradecemos el uso de nuestra aplicación.</p>`
+    //       + `<p>Hemos encontrado algunos errores que deben ser corregidos en brevedad. Por favor, Ingresa a ${appCnf.site.url}`
+    //       + ' y en la lista de tiendas aparecerá con un ícono rojo los problemas encontrados y una explicación de cómo puedes corregirlos.</p>'
+    //       + `<p>En ${appCnf.site.name} estamos comprometidos con la calidad de las tiendas aliadas que trabajan con nosotros,`
+    //       + ' recuerda que si tienes dudas adicionales, puedes contactarnos directamente por WhatsApp en el número'
+    //       + ` ${(_.find(appCnf.site.contacts, { slug: 'whatsapp' })).value}.</p>`,
+    //     },
+    //     { _id: store.userID._id },
+    //     cb);
+    //   }, cb);
+    // }],
   }, (err) => {
     if (err) {
       return next(err);
