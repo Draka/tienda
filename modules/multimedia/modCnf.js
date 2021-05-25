ddf8276e536ba1436afdbee39a357be821278cb1
Juan Carlos Martínez Rey <juan.mar@gmail.com>
2021-05-25 09:35:37 -0500

    dpi

--- modules/multimedia/modCnf.js
+++ modules/multimedia/modCnf.js
@@ -1,5 +1,5 @@
 const vars = {
-  imagesSizes: [{ x: 'default', y: 'default' }, { x: 48, y: 48 }, { x: 96, y: 96 }, { x: 196, y: 196 }, { x: 392, y: 392 }, { x: 968, y: 968 }],
+  imagesSizes: [{ x: 'default', y: 'default' }],
 
 };
 
